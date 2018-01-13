8476106944c02464eb79f154c00403e6875baa48
haoyingsong <haoyingsong@hotmail.com>
2018-01-13 22:07:34 +0800

    run cpu render mode on xp platform

--- Unosquare.FFME.Windows.Sample/App.xaml.cs
+++ Unosquare.FFME.Windows.Sample/App.xaml.cs
@@ -1,15 +1,25 @@
 ﻿namespace Unosquare.FFME.Windows.Sample
 {
-
+    using OsInfo;
+    using OsInfo.Extensions;
+    using System;
     using System.Windows;
+    using System.Windows.Media;
 
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            if(Environment.OSVersion.IsLessThan(OsVersion.Vista))
+            {
+                RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
+            }
 
-
+            base.OnStartup(e);
+        }
 
     }
 }
